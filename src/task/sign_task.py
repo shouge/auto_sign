4ae7f8dd4f076145459a7b4f7e87c4a615dbf292
shouge <shouge101@gmail.com>
2023-11-14 11:44:04 +0800

    fix: 修改判断是否手机签到方法

--- src/task/sign_task.py
+++ src/task/sign_task.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import time
 
 from ..checking.checker import Checker
+from ..checking.phone_sign_check import PhoneSignChecker
 from ..checking.random_check import RandomChecker
 from ..checking.today_sign_check import TodaySignChecker
 from ..checking.workday_check import WorkDayChecker
@@ -11,11 +12,11 @@ from ..sign.sign import Sign
 
 class SignTask:
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._sign: Sign = Sign()
         self._checker: Checker = WorkDayChecker()
         self._checker.set_next(TodaySignChecker(self._sign)).set_next(RandomChecker()).set_next(
-            TodaySignChecker(self._sign))
+            PhoneSignChecker(self._sign))
 
     def __call__(self, *args, **kwargs):
         check: bool = self._checker.check()
@@ -23,7 +24,8 @@ class SignTask:
             return None
 
         hour: int = time.localtime().tm_hour
-        if 6 < hour < 12:
+        is_sign_in: bool = 6 < hour < 12
+        if is_sign_in:
             self._sign.sing_in()
         else:
             self._sign.sing_out()
