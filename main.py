0d0218ea94907c4f8d45a2dd84943cbb1122e906
shouge <shouge101@gmail.com>
2023-11-14 11:53:24 +0800

    fix： 增加定时任务类型注解描述

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from apscheduler.triggers.cron import CronTrigger
 from src.task.sign_task import SignTask
 
 if __name__ == '__main__':
-    scheduler = Scheduler()
+    scheduler: Scheduler = Scheduler()
     task: SignTask = SignTask()
     scheduler.add_schedule(task.__call__,
                            CronTrigger.from_crontab(expr="*/2 9 * * *", timezone="Asia/Shanghai"), id="signIn")
