f5ff276816bcd2c2042bbff0a74387bc09da53ac
shouge <shouge101@gmail.com>
2023-11-14 11:44:31 +0800

    feat： 修改轮询时间间隔为每两分钟一次

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     scheduler = Scheduler()
     task: SignTask = SignTask()
     scheduler.add_schedule(task.__call__,
-                           CronTrigger.from_crontab(expr="*/1 9 * * *", timezone="Asia/Shanghai"), id="signIn")
+                           CronTrigger.from_crontab(expr="*/2 9 * * *", timezone="Asia/Shanghai"), id="signIn")
 
     scheduler.add_schedule(task.__call__,
-                           CronTrigger.from_crontab(expr="*/1 21 * * *", timezone="Asia/Shanghai"), id="signOut")
+                           CronTrigger.from_crontab(expr="*/2 21 * * *", timezone="Asia/Shanghai"), id="signOut")
     scheduler.start_in_background()
